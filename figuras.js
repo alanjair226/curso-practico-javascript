49b7bae1223112201374f1417c1c50fba79dc2aa
Alan Jair <alalmag2@gmail.com>
2022-06-09 15:53:34 -0500

    Calculos y diseño base terminado

--- figuras.js
+++ figuras.js
@@ -7,13 +7,20 @@ console.log("El perimetro del cuadrado mide. "+areaCuadrado);
 
 function calcularCuadrado(){
     const side= document.getElementById("inputSide");
-    console.log(side.value * 4);
+    document.getElementById('resSquarep').innerHTML = "El perimetro del cuadrado es: "+ side.value*4;
+    document.getElementById('resSquarea').innerHTML = "El perimetro del area es: "+ side.value*side.value;
 }
 function calcularTriangulo(){
-    const side= document.getElementById("inputSide");
-    console.log(side.value * 4);
+    const base= document.getElementById("inputBase");
+    const height= document.getElementById("inputHeight");
+    const side1=document.getElementById("ts1");
+    const side2=document.getElementById("ts2");
+    const side3=document.getElementById("ts3");
+    document.getElementById('resTrianglep').innerHTML = "El perimetro del cuadrado es: "+ (Number(side1.value)+Number(side2.value)+Number(side3.value));
+    document.getElementById('resTrianglea').innerHTML = "El area del Teiangulo es: "+ (base.value * height.value)/2;
 }
 function calcularCirculo(){
-    const side= document.getElementById("inputSide");
-    console.log(side.value * 4);
+    const radio= document.getElementById("inputRadio");
+    document.getElementById('resCirclep').innerHTML = "El perimetro del circulo es: "+ ((radio.value*2)*(3.1415));
+    document.getElementById('resCirclea').innerHTML = "El area del circulo es: "+ ((radio.value*radio.value)*3.1415);
 }
